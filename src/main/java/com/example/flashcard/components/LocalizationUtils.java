a6e3091fcfb4471a26d5faea4b5b75f0c7b5b559
ToanNguyen9602 <toannguyen9602@gmail.com>
2025-01-22 09:45:26 +0700

    commit add_dependency_pom

--- src/main/java/com/example/flashcard/components/LocalizationUtils.java
+++ src/main/java/com/example/flashcard/components/LocalizationUtils.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.LocaleResolver;
-
+import org.springframework.stereotype.Service;
 import java.util.Locale;
 
 @RequiredArgsConstructor
